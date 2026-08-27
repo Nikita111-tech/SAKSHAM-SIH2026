209113214deaf7f41d37b2586347358dc555cc20
Nikita111-tech <nikitasingh70582@gmail.com>
2026-08-27 07:50:46 +0530

    Fix package declaration in GeminiService.java

--- src/main/java/com/sih/saksham/service/GeminiService.java
+++ src/main/java/com/sih/saksham/service/GeminiService.java
@@ -1,4 +1,4 @@
-package com.saksham.service;
+package com.sih.saksham.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
