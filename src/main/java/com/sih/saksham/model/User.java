c329ac1089bd75281901b736de77c874a8d1d038
Nikita111-tech <nikitasingh70582@gmail.com>
2026-08-27 08:29:34 +0530

    Update User.java

--- src/main/java/com/sih/saksham/model/User.java
+++ src/main/java/com/sih/saksham/model/User.java
@@ -1,6 +1,7 @@
 package com.sih.saksham.model;
 import jakarta.persistence.*;
 @Entity
+@Table(name="users")
 public class User {
  @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
  private String name; private String email;
