61dccdffe4a92e5972af982ddb4dd3eeb9525d62
Nikita111-tech <nikitasingh70582@gmail.com>
2026-08-27 07:36:07 +0530

    Create UserRepository interface for User entity

---
+++ src/main/java/com/sih/saksham/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.saksham.repository;
+import com.saksham.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, Long> {}
